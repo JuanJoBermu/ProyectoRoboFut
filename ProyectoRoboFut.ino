1038068fdd218aafb54c0356b5a78fa1fc87b472
JuanJoBermu <juanjobermu.jb@gmail.com>
2019-02-16 15:38:48 -0500

    Giros hacia los lados

--- ProyectoRoboFut.ino
+++ ProyectoRoboFut.ino
@@ -18,17 +18,24 @@ void setup() {
 
 void loop() {
   
+  //der---> adelante (0)
+  //izq---> adelante (180)
   
   der.write(0);         //Hacia adelante
-  izq.write(180);
-  
+  izq.write(180);       
   delay(3000);
   
   der.write(180);      //Hacia atras
   izq.write(0);
+  delay(3000);
 
+  der.write(100);     //Giro a la derecha
+  izq.write(180);
+  delay(3000);
+
+  der.write(0);      //Giro a la izquierda
+  izq.write(80);
   delay(3000);
   
 
-    
 }
