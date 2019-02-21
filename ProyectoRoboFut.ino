70ce8df5fc4e2eda90b8954690d26cdbc9c4d0fe
JuanJoBermu <juanjobermu.jb@gmail.com>
2019-02-21 08:26:02 -0500

    Comandos de aumento de velocidad

--- ProyectoRoboFut.ino
+++ ProyectoRoboFut.ino
@@ -8,6 +8,9 @@ SoftwareSerial BT(RXBT,TXBT);
 int ServoD = 7;
 int ServoI = 4;
 char COMANDO;
+int velocidad = 90;
+int vel;
+int totd, toti;
 
 Servo der;
 Servo izq;
@@ -26,10 +29,12 @@ void loop() {
   
   //der---> adelante (0)
   //izq---> adelante (180)
+  
   if(BT.available()>0)
   {
-    COMANDO=BT.read();
+    COMANDO = BT.read();
   }
+  
   if(Serial.available()>0)
   {
     COMANDO = Serial.read();
@@ -38,28 +43,39 @@ void loop() {
   
     switch(COMANDO)
       {
+
+        case'+':
+          vel+=3;
+          totd=velocidad-vel;
+          toti=velocidad+vel;
+          der.write(totd);
+          izq.write(toti);
+          if (totd<0){ totd=0;}
+          if (toti>180){ toti=180;}
+        break;
+        
         case 'w': case'W':
-        der.write(0);         //Hacia adelante
-        izq.write(180); 
+          der.write(0);         //Hacia adelante
+          izq.write(180); 
         break;
     
         case 's': case'S':
-        der.write(180);      //Hacia atras
-        izq.write(0);
+          der.write(180);      //Hacia atras
+          izq.write(0);
         break;
     
         case 'a': case'A':
-        der.write(0);       //Giro a la izquierda
-        izq.write(80);
+          der.write(0);       //Giro a la izquierda
+          izq.write(80);
         break;
     
         case 'd': case'D':
-        der.write(100);     //Giro a la derecha
-        izq.write(180);
+          der.write(100);     //Giro a la derecha
+          izq.write(180);
         break;
     
         default:
-        der.write(90);      //Parar
-        izq.write(90);
+          der.write(90);      //Parar
+          izq.write(90);
       }
 }
